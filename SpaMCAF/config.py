9b4d7d83a1450149708d21237eebc281daf178f4
Wenwen Min <minlab@aliyun.com>
2025-04-02 18:18:33 +0800

    Delete SpaMCAF

--- SpaMCAF/config.py
+++
@@ -1,9 +0,0 @@
-
-def get_setting():
-    seed = 1234
-    batch_size = 1
-    learning_rate = 0.001
-    step_size = 500
-    gamma = 1
-    train_epoch = 1000
-    return seed, batch_size, learning_rate, step_size, gamma, train_epoch
